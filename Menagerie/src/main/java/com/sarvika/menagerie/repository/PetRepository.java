e9185c9e54e0cdd07f167afb0d0bfc8ce4483601
Suraj Das <surajdas3108@gmail.com>
2024-08-11 13:22:25 +0530

    changed the method from findAllBySpecies to findPetsBySpecies

--- Menagerie/src/main/java/com/sarvika/menagerie/repository/PetRepository.java
+++ Menagerie/src/main/java/com/sarvika/menagerie/repository/PetRepository.java
@@ -2,10 +2,10 @@ package com.sarvika.menagerie.repository;
 
 import com.sarvika.menagerie.model.Pet;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 
 public interface PetRepository extends JpaRepository<Pet, Integer> {
 
-    List<Pet> findAllBySpecies(String species);
+    List<Pet> findPetsBySpecies(String species);
+
 }
