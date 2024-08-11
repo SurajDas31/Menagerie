ef9ecd0d5a0bed2c15621006dfc8c560b4d0db80
Suraj Das <surajdas3108@gmail.com>
2024-08-11 15:04:19 +0530

    Handled order and sort in request parameter for getPetsById controller

--- Menagerie/src/main/java/com/sarvika/menagerie/exception/GlobalExceptionHandler.java
+++ Menagerie/src/main/java/com/sarvika/menagerie/exception/GlobalExceptionHandler.java
@@ -3,6 +3,8 @@ package com.sarvika.menagerie.exception;
 import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import com.sarvika.menagerie.model.API_Error;
 import com.sarvika.menagerie.model.Sex;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mapping.PropertyReferenceException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -34,7 +36,7 @@ public class GlobalExceptionHandler {
 
         API_Error api_error = null;
 
-        if (Sex.class.toString().equals(e.getTargetType().toString())){
+        if (Sex.class.toString().equals(e.getTargetType().toString())) {
             api_error = new API_Error();
             api_error.setStatus_code(400);
             api_error.setMessage("Invalid gender! Please choose 'm' or 'f'");
@@ -44,11 +46,36 @@ public class GlobalExceptionHandler {
         return new ResponseEntity(api_error, HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<API_Error> handleEntityNotFoundException(EntityNotFoundException e) {
+    @ExceptionHandler(PropertyReferenceException.class)
+    public ResponseEntity<Map<String, String>> handlePropertyReferenceException(PropertyReferenceException e) {
+
         API_Error api_error = new API_Error();
         api_error.setStatus_code(400);
         api_error.setMessage(e.getMessage());
+
+        return new ResponseEntity(api_error, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Map<String, String>> handleIllegalArgumentException(IllegalArgumentException e) {
+
+        API_Error api_error = new API_Error();
+        api_error.setStatus_code(400);
+        api_error.setMessage(e.getMessage());
+
+        return new ResponseEntity(api_error, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<API_Error> handleEntityNotFoundException(EntityNotFoundException e) {
+
+        API_Error api_error = null;
+        if (e.getMessage().contains("No enum constant org.springframework.data.domain.Sort.Direction")) {
+            // Handle invalid sort direction
+            api_error = new API_Error();
+            api_error.setStatus_code(400);
+            api_error.setMessage("Invalid input. Please type \"ASC\" or \"DESC\"");
+        }
 
         return new ResponseEntity<>(api_error, HttpStatus.NOT_FOUND);
     }
